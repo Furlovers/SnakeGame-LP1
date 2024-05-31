59e44d515f07d83a94879bd35ec84f700b6ab8c3
Rodrigo Morales <rodrigo.monasterios@outlook.com>
2024-05-31 14:31:05 -0300

    returning to sergiao user

--- src/app/screens/db/ConnFactory.java
+++ src/app/screens/db/ConnFactory.java
@@ -19,7 +19,7 @@ public class ConnFactory {
         String servidor = "localhost";
         String porta = "3306";
         String database = "TUTORIAL";
-        String usuario = "rodrigo";
+        String usuario = "root";
         String senha =  "admin";
         return DriverManager.getConnection("jdbc:mysql://" + servidor+":" + porta + "/" + database + "?user="+ usuario + "&password=" + senha);
     }
